a9a7003163a669610548aad363552e0f7aa4073a
RahulGupta <rahulrgd21@gmail.com>
2024-01-13 16:10:01 +0530

    Completed Step-28, Connected H2 database with spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO user_details(id, name, birth_date)
+VALUES(10001, 'Rahul', current_date());
+
+INSERT INTO user_details(id, name, birth_date)
+VALUES(10002, 'Rahi', current_date());
+
+INSERT INTO user_details(id, name, birth_date)
+VALUES(10003, 'Satendra', current_date());
